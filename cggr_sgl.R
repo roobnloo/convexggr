30a38e65477e984543fef36d06ce9bc7591216ae
Robin Liu <71776622+roobnloo@users.noreply.github.com>
2022-10-24 11:45:00 -0700

    begin cggr_sgl with ridge penalty on mean

--- cggr_sgl.R
+++ cggr_sgl.R
@@ -6,12 +6,13 @@ source("utils.R")
 #' @param lambda_g numeric penalty for mean vector
 #' @param lambda_b_seq vector of d penalties for each response
 #' @param alpha_seq values between 0 and 1 that determines sparse group lasso penalty
-cggr_sgl <- function(responses, covariates, alpha_seq, reg_gamma = 0.01,
+cggr_sgl <- function(responses, covariates, alpha_seq, regmean = 0.01,
                      reparametrize = T, lambda_seq = NULL, tune = T) {
   stopifnot(is.matrix(responses),
             is.matrix(covariates),
             nrow(responses) == nrow(covariates),
-            all(alpha_seq >= 0, alpha_seq <= 1))
+            all(alpha_seq >= 0, alpha_seq <= 1),
+            tune || !is.null(lambda_seq))
 
   d <- ncol(responses)
   p <- ncol(covariates)
@@ -27,35 +28,9 @@ cggr_sgl <- function(responses, covariates, alpha_seq, reg_gamma = 0.01,
   est_vars <- vector(length = d)
 
   for (node in seq_len(d)) {
-    y <- scale(responses[, node], scale = F)
-    mx <- cbind(responses[, -node],
-                interaction_mx(responses[, -node], covariates))
-
-    # There are (p + 1) groups and the size of each group is d-1
-    grp_idx <- rep(1:(p + 1), each = d-1)
-
-    if (tune) {
-      cv_result <- cv.sparsegl(mx, y, grp_idx,
-                               asparse = alpha_seq,
-                               # lambda = lambda_seq[node],
-                               nlambda = 100,
-                               # pf_group = c(0, rep(sqrt(d-1), p)),
-                               pf_group = c(0, rep(1, p)),
-                               nfolds = 5,
-                               intercept = F,
-                               standardize = F)
-    } else {
-     cv_result <- sparsegl(mx, y, grp_idx, lambda = lambda_seq[node], asparse = 0.75,
-                           intercept = F, standardize = F, pf_group = c(0, rep(1, p)))
-    }
-
-    # gamma_mx[node, ] <- result$gamma_j
-    # ignore the intercept fitted below with -1
-    lambda <- ifelse(is.null(lambda_seq), "lambda.min", lambda_seq[node])
-    beta <- as.numeric(coef(cv_result, s = lambda))[-1]
-
-    rss <- sum((y - predict(cv_result, mx,s = lambda))^2)
-    num_nz <- sum(abs(beta) > 1e-10) # add gamma to this later
+    result <- cggr_sgl_node(node, responses, covariates, lambda = 0.03, alpha = 0.75)
+    rss <- sum(result$resid^2)
+    num_nz <- sum(abs(result$beta) > 1e-10) + sum(abs(result$gamma) > 1e-10)
     est_vars[node] <- rss  / (nrow(responses) - num_nz)
 
     # Index the columns of each beta matrix to fill.
@@ -66,11 +41,13 @@ cggr_sgl <- function(responses, covariates, alpha_seq, reg_gamma = 0.01,
       bh_idx <- (h - 1) * (d - 1) + seq_len(d - 1)
 
       if (reparametrize) {
-        beta_mxs[[h]][node, beta_mx_idx] <- beta[bh_idx]
+        beta_mxs[[h]][node, beta_mx_idx] <- result$beta[bh_idx]
       } else {
-        beta_mxs[[h]][node, beta_mx_idx] <- -beta[bh_idx] / est_vars[node]
+        beta_mxs[[h]][node, beta_mx_idx] <- -result$beta[bh_idx] / est_vars[node]
       }
     }
+
+    gamma_mx[node,] <- result$gamma
   }
 
   symmed_beta_mxs <-  map(seq_len(p + 1), ~ matrix(nrow = d, ncol = d))
@@ -82,4 +59,85 @@ cggr_sgl <- function(responses, covariates, alpha_seq, reg_gamma = 0.01,
               beta_mxs = symmed_beta_mxs,
               asym_beta_mx = beta_mxs,
               sigma_sq = est_vars))
+}
+
+cv.cggr_sgl_node <- function(node, resp, covar, nlambda, lambda_factor = 1e-4,
+                             asparse,
+                             initbeta = NULL, initgamma = NULL,
+                             nfold = 5,
+                             regmean = 0.01,
+                             maxiter = 1000, tol = 1e-5) {
+  d <- ncol(resp)
+  p <- ncol(covar)
+  num <- nrow(resp)
+  beta <- initbeta
+  gamma <- initgamma
+  if (is.null(beta)) {
+    beta <- rep(0, (p+1)*(d-1))
+  }
+  if (is.null(gamma)) {
+    gamma <- rep(0, p)
+  }
+
+  y <- scale(resp[, node], scale = F)
+  W <- cbind(resp[, -node], interaction_mx(resp[, -node], covar))
+
+  lam_max <- max(abs(t(W) %*% y))
+  lambda_seq <- lam_max * exp(seq(log(1), log(lambda_factor)))
+
+  foldids <- split(seq_len(num), cut(seq_along(num), nfold, labels = F))
+}
+
+cggr_sgl_node <- function(node, resp, covar, lambda, alpha,
+                          initbeta = NULL, initgamma = NULL,
+                          regmean = 0.01,
+                          maxiter = 1000, tol = 1e-5) {
+  d <- ncol(resp)
+  p <- ncol(covar)
+  num <- nrow(resp)
+  beta <- initbeta
+  gamma <- initgamma
+  if (is.null(beta)) {
+    beta <- rep(0, (p+1)*(d-1))
+  }
+  if (is.null(gamma)) {
+    gamma <- rep(0, p)
+  }
+
+  y <- scale(resp[, node], scale = F)
+  W <- cbind(resp[, -node], interaction_mx(resp[, -node], covar))
+
+  # There are (p + 1) groups and the size of each group is d-1
+  grp_idx <- rep(1:(p + 1), each = d-1)
+
+  # residual vector
+  r <- y - covar %*% gamma - W %*% beta
+  objval <- vector(length = 1000) # keep track fo objective values
+
+  for (i in seq_len(maxiter)) {
+    objval[i] <- sum(r^2) / (2 * num) + regmean * sum(gamma^2) +
+                   alpha * lambda * sum(abs(beta[seq_len(d-1)])) +
+                   (1 - alpha) * lambda * sum(beta[-seq_len(d-1)]^2)
+    if (i > 1 && abs(objval[i] - objval[i-1]) < tol) {
+      objval <- objval[1:i]
+      break
+    }
+
+    # sparsegl step
+    rbeta <- r + W %*% beta # partial residual for beta
+    sgl <- sparsegl(W, rbeta, grp_idx, lambda = lambda, asparse = alpha,
+                    intercept = F, standardize = F, pf_group = c(0, rep(1, p)))
+    beta <- as.numeric(coef(sgl, s = lambda)[-1])
+    r <- rbeta - W %*% beta
+
+    # ridge step
+    rgamma <- r + covar %*% gamma
+    gamma <- solve(t(covar) %*% covar + diag(regmean, p, p)) %*% t(covar) %*% rgamma
+    r <- rgamma - covar %*% gamma
+  }
+
+  return(list(beta = beta,
+              gamma = gamma,
+              objval = objval,
+              resid = r))
 }
